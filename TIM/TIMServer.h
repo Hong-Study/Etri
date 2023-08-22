637a3b042e77fd8e8c5bb0fa26f97873b42fbd32
Hong-Study <48054868+Hong-Study@users.noreply.github.com>
2023-08-23 03:11:14 +0900

    시발

--- TIM/TIMServer.h
+++ TIM/TIMServer.h
@@ -18,7 +18,7 @@ public:
 	void PopList(SessionRef ref);
 	
 
-	bool PushPairingList(TifdRef tifd, TirdRef tird, int32 distance);
+	void PushPairingList(TifdRef tifd, TirdRef tird, int32 distance);
 	void PopPairingList(int32 pairingId, SessionRef session);
 
 	void GetPossiblePairingList(pair<float, float> tifdLocation, vector<PossiblePairingList>& lists);
